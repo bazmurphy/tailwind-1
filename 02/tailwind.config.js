bcf1d34f14b230b31ff31a1adc0c13cc6c9e5491
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-07-18 17:55:51 +0100

    02b

--- 02/tailwind.config.js
+++ 02/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./build/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      // we add our custom media query
+      screens: {
+        // the width has to be a minimum of 3 to 2
+        widescreen: { raw: "(min-aspect-ratio: 3/2)" },
+        // the height has to be 2x the width
+        tallscreen: { raw: "(min-aspect-ratio: 1/2)" },
+      },
+    },
   },
   plugins: [],
 };
