a663f68eb84a8b300122cbb68099aa7ff2bf8bb2
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-07-19 19:59:34 +0100

    02c

--- 02/tailwind.config.js
+++ 02/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html"],
+  // if there is a class that isn't mentioned in the html but is mentioned in a JS file
+  // then we need to tell it to also look at the JS files so they are included in the built
+  content: ["./build/*.html", "./build/js/*.js"],
   theme: {
     extend: {
       // we add our custom media query
@@ -8,9 +10,20 @@ module.exports = {
         // the width has to be a minimum of 3 to 2
         widescreen: { raw: "(min-aspect-ratio: 3/2)" },
         // the height has to be 2x the width
-        tallscreen: { raw: "(min-aspect-ratio: 1/2)" },
+        tallscreen: { raw: "(min-aspect-ratio: 2/3)" },
+      },
+      // we can add our own keyframes
+      keyframes: {
+        "open-menu": {
+          "0%": { transform: "scaleY(0)" },
+          "100%": { transform: "scaleY(1)" },
+        },
+      },
+      // and add our own animation
+      animation: {
+        "open-menu": "open-menu 0.3s ease-in-out forwards",
       },
     },
   },
-  plugins: [],
+  plugins: ['prettier-plugin-tailwindcss'],
 };
